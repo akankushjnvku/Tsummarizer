394b42f98c60d83441d86667eaaa0f24928f0719
akankushjnvku <71630336+akankushjnvku@users.noreply.github.com>
2023-08-12 22:03:56 +0530

    requirements and installaion done

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__= "0.0.0"
+
+REPO_NAME = "Tsummarizer"
+AUTHOR_USER_NAME = "akankushjnvku"
+SRC_REPO = "textsummarizer"
+AUTHOR_EMAIL = "akankush2000@gmail.com"
+
+
+
+setuptools.setup(
+    name = REPO_NAME,
+    version = __version__,
+    author = AUTHOR_USER_NAME,
+    author_email = AUTHOR_EMAIL,
+    description = "Text Summarizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/akankushjnvku/Tsummarizer",
+    project_urls={
+        "Bug Tracker": "https://github.com/akankushjnvku/Tsummarizer/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+    
+)
